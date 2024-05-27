ffa549a2a054f7724aed8b25a21fd6defd29626f
enwelum chiamaka <enwelumchiamaka@gmail.com>
2024-05-27 06:38:16 +0100

    App Set up and installing dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Nav_bar from './Components/Nav_bar';
+
+
+
+function App() {
+
+  return (
+    <>
+      <Nav_bar />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Nav_bar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav_bar = () => {
+  return (
+    <div className='text-center'>
+      Home Page
+    </div>
+  )
+}
+
+export default Nav_bar
